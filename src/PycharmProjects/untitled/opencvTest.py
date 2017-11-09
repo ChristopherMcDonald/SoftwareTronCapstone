ecd08aadcf1d1e263c31cce6fa37abedf60e07b8
hamels2 <sam2hamel@gmail.com>
2017-11-09 17:48:17 -0500

    added xy coordinate tracking

--- src/PycharmProjects/untitled/opencvTest.py
+++ src/PycharmProjects/untitled/opencvTest.py
@@ -10,7 +10,7 @@ print(cv2.__version__)
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--buffer", type=int, default=64,
+ap.add_argument("-b", "--buffer", type=int, default=32,
 	help="max buffer size")
 args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
@@ -24,7 +24,10 @@ OrangeUpper = (30,200,300)
 
 
 
+
 pts = deque(maxlen=args["buffer"])
+counter =0
+(dX, dY) = (0,0);
 cap = cv2.VideoCapture(0)
 print(cap.get(3))
 print(cap.get(4))
@@ -58,13 +61,13 @@ while(True):
         ##mask = cv2.erode(mask, None, iterations=2)
         ##mask = cv2.dilate(mask, None, iterations=2)
         circles = np.uint16(np.around(circles))
-        print(circles)
+
         for i in circles[0, :]:
             # draw the outer circle
             cv2.circle(thresh, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
             cv2.circle(thresh, (i[0], i[1]), 2, (0, 0, 255), 3)
-        print("\n")
+
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
@@ -90,11 +93,11 @@ while(True):
             cv2.circle(frame, (int(x), int(y)), int(radius),
                        (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
+            pts.appendleft(center)
     # update the points queue
-    pts.appendleft(center)
+
     # loop over the set of tracked points
-    for i in range(1, len(pts)):
+    for i in np.arange(1, len(pts)):
         # if either of the tracked points are None, ignore
         # them
         if pts[i - 1] is None or pts[i] is None:
@@ -104,11 +107,29 @@ while(True):
         # draw the connecting lines
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    # check to see if enough points have been accumulated in
+    # the buffer
+        if counter >= 10 and i == 1 and pts[-10] is not None:
+            # compute the difference between the x and y
+            # coordinates and re-initialize the direction
+            # text variables
+            dX = pts[-10][0] - pts[i][0]
+            dY = pts[-10][1] - pts[i][1]
+            (dirX, dirY) = ("", "")
+
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
    ## cv2.imshow("shape",thresh)
     key = cv2.waitKey(1) & 0xFF
+    counter+=1
 
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
