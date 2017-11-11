f89ce9c0ca283acace927fed7df4cdbd6fe91786
Christopher McDonald <mail@christophermcdonald.me>
2017-11-10 19:17:38 -0500

    changed vars to lower camel and added javadoc comments

--- src/ShootingModel/src/ShootingModel.java
+++ src/ShootingModel/src/ShootingModel.java
@@ -1,77 +1,108 @@
-
+/**
+ * Represents the Shooting Mechanism, used for determining valid shot parameters 
+ * @author Harit Patel
+ * @version 1.1
+ *
+ * TODO items:
+ * - add Calculate Y Plane Angle method
+ */
 public class ShootingModel {
-	
+
 	// Instantiate necessary variables
 	public double distance;
-	public double x_distance;
-	public double y_distance;
-	public double x_plane_angle;
-	public double y_initial_height;
-	public double y_plane_angle = 45;
-	public double g = 9.8;
+	public double xDistance;
+	public double yDistance;
+	public double xPlaneAngle;
+	public double yInitialHeight;
+	public double yPlaneAngle = 45;
+	public double gravity = 9.8;
 	public double velocity;
-	
-	//Calculates the distance in x plane from the shooter to the landing point
-	public double calculate_x_distance(double landing_x_coord){
+
+	/**
+	 * Calculates the distance in x plane from the shooter to the landing point
+	 * @param  double landingXCoord [TODO description]
+	 * @return        [TODO description]
+	 */
+	public double calculateXDistance(double landingXCoord){
 		//The 0.38125 is the centre of the board in x-plane; location of the shooter.
-		double x_dist = Math.abs(0.7625 - landing_x_coord);
-		return x_dist;
+		double xDist = Math.abs(0.7625 - landingXCoord);
+		return xDist;
 	}
-	
-	//Calculates the distance in y plane from the shooter to the landing point
-	public double calculate_y_distance(double landing_y_coord){
+
+	/**
+	 * Calculates the distance in y plane from the shooter to the landing point
+	 * @param  double landingYCoord [TODO description]
+	 * @return        [TODO description]
+	 */
+	public double calculateYDistance(double landingYCoord){
 		//The location of the shooter is at 0 m from the edge
-		double y_dist = Math.abs(landing_y_coord - 0);
-		return y_dist;
+		double yDist = Math.abs(landingYCoord - 0);
+		return yDist;
 	}
-	
-	/*
-	 * This is for when you allow angle to change - this will need to change the speed
-	 * Leave for Iteration 2
-	 * 
-	 * public double calculate_y_plane_angle(){
-		return 0.0;
-	}
-	*/
-	
+
 	//Calculates the total distance from the shooter to the landing point
-	public double calculate_distance(double landing_x_coord, double landing_y_coord){
-		x_distance = calculate_x_distance(landing_x_coord);
-		y_distance = calculate_y_distance(landing_y_coord);	
-		double dist_squared = Math.pow(x_distance, 2)+Math.pow(y_distance, 2);
-		double total_distance = Math.sqrt(dist_squared);
-		System.out.println("Total travel distance: "+total_distance+" meters");
-		return total_distance;
+	/**
+	 * Calculates the total distance from the shooter to the landing point
+	 * @param landingXCoord TODO descr
+	 * @param landingYCoord TODO descr
+	 * @return 
+	 */
+	public double calculateDistance(double landingXCoord, double landingYCoord){
+		xDistance = calculateXDistance(landingXCoord);
+		yDistance = calculateYDistance(landingYCoord);
+		double distSquared = Math.pow(xDistance, 2) + Math.pow(yDistance, 2);
+		double totalDistance = Math.sqrt(distSquared);
+		System.out.println("Total travel distance: " + totalDistance + " meters");
+		return totalDistance;
 	}
-	
-	public double calculate_x_angle(double y_dist, double x_dist){
-		double angle = Math.atan(y_dist/x_dist);
-		System.out.println("Angle in the X Plane: "+Math.toDegrees(angle));
+
+	/**
+	 * TODO
+	 * @param yDist TODO
+	 * @param xDist TODO
+	 * @return TODO
+	 */
+	public double calculateXAngle(double yDist, double xDist){
+		double angle = Math.atan(yDist/xDist);
+		System.out.println("Angle in the X Plane: " + Math.toDegrees(angle));
 		return angle;
 	}
-	
+
 	//TODO: Change Y angle based on velocity as an input
-	public double calculate_y_angle(){
-		System.out.println("Angle in the Y Plane: "+y_plane_angle);
-		return y_plane_angle;
+	/**
+	 * TODO
+	 * @return TODO
+	 */
+	public double calculateYAngle(){
+		System.out.println("Angle in the Y Plane: " + yPlaneAngle);
+		return yPlaneAngle;
 	}
-	
-	//Constructor - define variables here
-	public ShootingModel(double initial_height, double landing_x_coord, double landing_y_coord){
-		y_initial_height = initial_height;
-		distance = calculate_distance(landing_x_coord, landing_y_coord);
-		x_plane_angle = calculate_x_angle(x_distance, y_distance);
-		double y_angle = calculate_y_angle();
-		velocity = calculate_velocity();
-	}
-	
-	public double calculate_velocity(){
-		double vel = Math.sqrt((Math.pow(distance, 2)*g)/(2*Math.pow(Math.cos(y_plane_angle),2)*(y_initial_height+distance*Math.tan(y_plane_angle))));
+
+	/**
+	 * TODO
+	 * @return TODO
+	 */
+	public double calculateVelocity(){
+		double vel = Math.sqrt((Math.pow(distance, 2)*gravity)/(2*Math.pow(Math.cos(yPlaneAngle),2)*(yInitialHeight+distance*Math.tan(yPlaneAngle))));
 		System.out.println("Velocity: "+vel+" m/s");
 		return vel;
 	}
 	
+	/**
+	 * ShootingModel Constructor
+	 * @param initialHeight
+	 * @param landingXCoord
+	 * @param landingYCoord
+	 */
+	public ShootingModel(double initialHeight, double landingXCoord, double landingYCoord){
+		yInitialHeight = initialHeight;
+		distance = calculateDistance(landingXCoord, landingYCoord);
+		xPlaneAngle = calculateXAngle(xDistance, yDistance);
+		double y_angle = calculateYAngle();
+		velocity = calculateVelocity();
+	}
+
 	public static void main(String []args){
-		ShootingModel shooter = new ShootingModel(0.0762, 0.38125, 2.055);		
-	}	
+		ShootingModel shooter = new ShootingModel(0.0762, 0.38125, 2.055);
+	}
 }
