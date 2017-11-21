9cf8a8c61fe6033217f8dc37711920c9758cde1c
Christopher McDonald <mail@christophermcdonald.me>
2017-11-20 22:08:12 -0500

    added rise fall detection

--- src/PycharmProjects/untitled/opencvTest.py
+++ src/PycharmProjects/untitled/opencvTest.py
@@ -21,9 +21,22 @@ Orange1Lower = (0,160,210)
 OrangeLower = (0,145,200)
 OrangeUpper = (30,200,300)
 
+# global variables
+BLUE = [255,0,0];
+GREEN = [0,255,0];
+RED = [0,0,255];
+BORDERWIDTH = 5;
 
+# up/down travel variables
+minMax = -1;
+rising = False;
+init = False;
 
+# ball state FSM
+fsm = 0; # 0 - initial fall, 1 - rise, 2 - out of frame
 
+# detect mode, will exit on return (successful or not)
+detect = True;
 
 pts = deque(maxlen=args["buffer"])
 counter =0
@@ -34,8 +47,7 @@ print(cap.get(4))
 
 ok,frame = cap.read()
 
-
-while(True):
+while(True and not (fsm != 0 and detect)):
     # grab the current frame
     (grabbed, frame) = cap.read()
     img = cap.read()
@@ -74,7 +86,10 @@ while(True):
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
-
+    if(rising):
+        borderColour = GREEN;
+    else:
+        borderColour = RED;
 
     # only proceed if at least one contour was found
     if len(cnts) > 0:
@@ -83,6 +98,25 @@ while(True):
         # centroid
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
+        if(not init):
+            minMax = y;
+            init = True;
+        else:
+            if(rising):
+                if(y < minMax):
+                    minMax = y;
+                elif(y > minMax + 25):
+                    minMax = y;
+                    rising = False;
+                    borderColour = RED;
+            else:
+                if(y < minMax - 25):
+                    minMax = y;
+                    rising = True;
+                    borderColour = GREEN;
+                    fsm = 1;
+                elif(y > minMax):
+                    minMax = y;
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
@@ -94,7 +128,8 @@ while(True):
                        (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             pts.appendleft(center)
-    # update the points queue
+    elif(init):
+        fsm = 2;
 
     # loop over the set of tracked points
     for i in np.arange(1, len(pts)):
@@ -109,7 +144,7 @@ while(True):
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
     # check to see if enough points have been accumulated in
     # the buffer
-        if counter >= 10 and i == 1 and pts[-10] is not None:
+        if counter >= 10 and i == 10 and pts[-10] is not None:
             # compute the difference between the x and y
             # coordinates and re-initialize the direction
             # text variables
@@ -123,10 +158,17 @@ while(True):
                 (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
                 0.35, (0, 0, 255), 1)
 
-
-
     # show the frame to our screen
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", mask)
+    border = cv2.copyMakeBorder(
+        frame,
+        top = BORDERWIDTH,
+        bottom = BORDERWIDTH,
+        left = BORDERWIDTH,
+        right = BORDERWIDTH,
+        borderType = cv2.BORDER_CONSTANT,
+        value = borderColour);
+    cv2.imshow('border', border)
    ## cv2.imshow("shape",thresh)
     key = cv2.waitKey(1) & 0xFF
     counter+=1
@@ -135,5 +177,9 @@ while(True):
     if key == ord("q"):
         break
 
+if(fsm == 1):
+    print("success");
+else:
+    print("fail");
 cap.release()
 cv2.destroyAllWindows()
