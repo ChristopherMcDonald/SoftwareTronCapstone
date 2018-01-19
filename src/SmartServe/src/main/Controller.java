b42628e8fe02d9a2e0f9588a687ba06ef57243c3
Christopher McDonald <mail@christophermcdonald.me>
2018-01-19 10:36:00 -0500

    CVcontroller code done

--- src/SmartServe/src/main/Controller.java
+++ src/SmartServe/src/main/Controller.java
@@ -1,14 +1,7 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 import adt.*;
 import controllers.*;
 
