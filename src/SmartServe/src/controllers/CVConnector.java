43a3787e2b0a4339cc2010f8e01ca6bbe9273029
Christopher McDonald <mail@christophermcdonald.me>
2018-01-18 20:15:56 -0500

    comm between CV and SS example

--- src/SmartServe/src/controllers/CVConnector.java
+++ src/SmartServe/src/controllers/CVConnector.java
@@ -1,8 +1,35 @@
 package controllers;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class CVConnector {
 	
 	private int port; // the port value if successfully connects
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// below is an example of how to in/out comm with python
+		int port = 8013;
+		Socket clientSocket = new Socket("localhost", port);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		
+		outToServer.writeUTF("TEST");
+		clientSocket.close();
+		
+		ServerSocket server = new ServerSocket(port + 1);
+		Socket client = server.accept();
+        System.out.println("got connection on port 8014");
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        String fromClient = in.readLine();
+        System.out.println("received: " + fromClient);
+        server.close();
+	}
 	
 	/**
 	 * attempts to connect to the CV over some port <code>port</code>
--- src/SmartServe/src/main/Controller.java
+++ src/SmartServe/src/main/Controller.java
@@ -1,7 +1,15 @@
 package main;
 
-import adt.Mode;
-import adt.ShootingParameters;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import adt.*;
 import controllers.*;
 
 /**
@@ -15,19 +23,56 @@ public class Controller {
 	private ShootingParameters sp;
 	private Mode m;
 	
+	// sockets
+	ServerSocket welcomeSocket;
+	
 	// controllers
 	private ArduinoController ardController;
+	private static final int ARD_PORT = 8000;
+	
 	private CVConnector cvController;
+	private static final int CV_PORT = 8010;
+	private static final int CV_IN_PORT = 8011;
+	
 	private ShotRecommendationController srController;
+	private static final int SR_PORT = 8020;
+	
 	private SQLConnector sqlController;
+	private static final int SQL_PORT = 3306;
 	
 	/**
 	 * initiates all connectors
 	 * @return successful boot
 	 */
 	public boolean boot() {
-		// TODO implement
-		return false;
+		ardController = new ArduinoController();
+		if(!ardController.test(ARD_PORT)) {
+			return false;
+		}
+		
+		cvController = new CVConnector();
+		if(!cvController.connect(CV_PORT)) {
+			return false;
+		}
+		
+		srController = new ShotRecommendationController();
+		if(!srController.connect(SR_PORT)) {
+			return false;
+		}
+		
+		sqlController = new SQLConnector();
+		if(!sqlController.connect(SQL_PORT)) {
+			return false;
+		}
+		
+		try {
+			welcomeSocket = new ServerSocket(CV_IN_PORT);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 	
 	/**
@@ -37,7 +82,13 @@ public class Controller {
 	public void startTraining(Mode m) {
 		this.m = m;
 		while(true) {
+			Shot s = srController.getRecommendation();
 			
+			// TODO trade shot for position with shooting model
+			// TODO optimize shots
+			
+			cvController.start();
+			ardController.shoot(0.0f, 0.0f, 0.0f); // TODO: fill
 		}
 	}
 	
