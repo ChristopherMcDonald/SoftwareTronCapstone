42b540bf9b7657afef3ba83f498d6ddd968e2171
Christopher McDonald <mail@christophermcdonald.me>
2018-01-24 18:21:24 -0500

    working for detecting 100 frames

--- src/ComputerVision/cv.py
+++ src/ComputerVision/cv.py
@@ -1,10 +1,8 @@
 import cv2
 from collections import deque
 import numpy as np
-import sys, argparse, imutils
-from datetime import datetime
+import sys, argparse, imutils;
 import socket, time;
-from enum import Enum;
 
 ap = argparse.ArgumentParser();
 ap.add_argument(
@@ -18,8 +16,8 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-Orange1Upper = (28,216,294)
-Orange1Lower = (6,142,161)
+Orange1Upper = (32,204,248)
+Orange1Lower = (9,105,130)
 # OrangeLower = (0,145,200)
 # OrangeUpper = (30,200,300)
 
@@ -35,11 +33,14 @@ rising = False
 init = False
 
 # ball state FSM
-fsm = 0 # 0 - waiting request, 1 - detecting, 2 - active ball, 3 - descent
+fsm = 0 # 0 - initial fall, 1 - rise, 2 - out of frame
+
+# detect mode, will exit on return (successful or not)
+detect = False;
 
 # global variables
 HOST = "localhost";
-PORT = 9003;
+PORT = 8013;
 
 socketIn = socket.socket();             # handles incoming requests
 socketIn.bind((HOST, PORT));            # binds to a host and port
@@ -53,164 +54,178 @@ print(cap.get(3))
 print(cap.get(4))
 
 ok,frame = cap.read()
-while(True):
+loop = 0;
+first = True;
+while(True and not (fsm != 0 and detect)):
     
-    if(fsm == 0):                           # waiting on connection
+    if(loop % 100 == 0):
+        
+        if(False == first):
+            print("sending ret...");
+            socketOut.send(b'RETURNED\n');
+        else:
+            first = False;
         
         conn, addr = socketIn.accept();     # loop will freeze on this command, runs when SmartServe
         print("Got connection from", addr); # sends a request
         msg = conn.recv(1024);              # parses msg... could be "TEST", "DETECT"
     
         # DEBUGGING
-        # msg = b'DETECT'
-        # print(msg);
+        print(msg);
+    
+        # DEBUGGING
+        time.sleep(1);
+    
+        # socket for outgoing messages
+        socketOut = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        socketOut.connect((HOST, PORT + 1));
+    
+        # DEBUGGING
+        time.sleep(1);
     
         if("TEST" in msg.decode("UTF8")):
-            # DEBUGGING
-            time.sleep(1);
-            
-            # socket for outgoing messages
-            socketOut = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-            socketOut.connect((HOST, PORT + 1));
-            
+            loop = 99;
             socketOut.send(b'ALLGOOD\n');
         else:
-            start = datetime.now();
-            fsm = 1;
+            loop = 0;
+            first = False;
     
-    if(fsm == 1 or fsm == 2 or fsm == 3):
-        
-        if((datetime.now() - start).seconds > 10):
-            # socket for outgoing messages
-            socketOut = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-            socketOut.connect((HOST, PORT + 1));
-            
-            socketOut.send(b'BAD\n');
-            fsm = 0;
-            pts = deque(maxlen=args["buffer"])
+    loop = loop + 1;
+    # grab the current frame
+    (grabbed, frame) = cap.read()
+    img = cap.read()
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, Orange1Lower, Orange1Upper)
+    thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(thresh, (9, 9), 0)
+
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 2000, 25, 250, 10, 10)  # ret=[[Xpos,Ypos,Radius],...]
+
+    if(circles is not None):
+        ##mask = cv2.inRange(hsv, OrangeLower, OrangeUpper)
+        ##mask = cv2.erode(mask, None, iterations=2)
+        ##mask = cv2.dilate(mask, None, iterations=2)
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(thresh, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(thresh, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    if(rising):
+        borderColour = GREEN;
+    else:
+        borderColour = RED;
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        if(not init):
+            minMax = y
+            init = True
         else:
-            # grab the current frame
-            (grabbed, frame) = cap.read()
-            img = cap.read()
-            
-            # resize the frame, blur it, and convert it to the HSV
-            # color space
-            frame = imutils.resize(frame, width=600)
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            if(rising):
+                if(y < minMax):
+                    minMax = y
+                elif(y > minMax + 30):
+                    minMax = y
+                    rising = False
+                    borderColour = RED
+            else:
+                if(y < minMax - 30):
+                    minMax = y
+                    rising = True
+                    borderColour = GREEN
+                    fsm = 1
+                elif(y > minMax):
+                    minMax = y
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-            # construct a mask for the color "green", then perform
-            # a series of dilations and erosions to remove any small
-            # blobs left in the mask
-            mask = cv2.inRange(hsv, Orange1Lower, Orange1Upper)
-            thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(thresh, (9, 9), 0)
+        # only proceed if the radius meets a minimum size
+        if radius > 2:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                       (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            pts.appendleft(center)
+    elif(init):
+        fsm = 2
 
-            mask = cv2.erode(mask, None, iterations=2)
-            mask = cv2.dilate(mask, None, iterations=2)
+    # loop over the set of tracked points
+    for i in np.arange(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
 
-            circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 2000, 25, 250, 10, 10)  # ret=[[Xpos,Ypos,Radius],...]
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    # check to see if enough points have been accumulated in
+    # the buffer
+        if counter >= 10 and i == 10 and pts[-10] is not None:
+            # compute the difference between the x and y
+            # coordinates and re-initialize the direction
+            # text variables
+            dX = pts[-10][0] - pts[i][0]
+            dY = pts[-10][1] - pts[i][1]
+            (dirX, dirY) = ("", "")
 
-            if(circles is not None):
-                ##mask = cv2.inRange(hsv, OrangeLower, OrangeUpper)
-                ##mask = cv2.erode(mask, None, iterations=2)
-                ##mask = cv2.dilate(mask, None, iterations=2)
-                circles = np.uint16(np.around(circles))
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
 
-                for i in circles[0, :]:
-                    # draw the outer circle
-                    cv2.circle(thresh, (i[0], i[1]), i[2], (0, 255, 0), 2)
-                    # draw the center of the circle
-                    cv2.circle(thresh, (i[0], i[1]), 2, (0, 0, 255), 3)
+    # show the frame to our screen
+    cv2.imshow("Frame", mask)
+    border = cv2.copyMakeBorder(
+        frame,
+        top = BORDERWIDTH,
+        bottom = BORDERWIDTH,
+        left = BORDERWIDTH,
+        right = BORDERWIDTH,
+        borderType = cv2.BORDER_CONSTANT,
+        value = borderColour)
+    cv2.imshow('border', border)
+   ## cv2.imshow("shape",thresh)
+    key = cv2.waitKey(1) & 0xFF
+    counter += 1
 
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
 
-            # find contours in the mask and initialize the current
-            # (x, y) center of the ball
-            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
-            center = None
+if(fsm == 1):
+    txt = "Success!"
+else:
+    txt = "Fail!"
 
-            # only proceed if at least one contour was found
-            if len(cnts) > 0:
-                # find the largest contour in the mask, then use
-                # it to compute the minimum enclosing circle and
-                # centroid
-                c = max(cnts, key=cv2.contourArea)
-                ((x, y), radius) = cv2.minEnclosingCircle(c)
-                M = cv2.moments(c)
-                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-                # only proceed if the radius meets a minimum size
-                if radius > 2:
-                    # draw the circle and centroid on the frame,
-                    # then update the list of tracked points
-                    cv2.circle(frame, (int(x), int(y)), int(radius),
-                               (0, 255, 255), 2)
-                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                    pts.appendleft(center)
-
-            # loop over the set of tracked points
-            for i in np.arange(1, len(pts)):
-                # if either of the tracked points are None, ignore
-                # them
-                if pts[i - 1] is None or pts[i] is None:
-                    continue
-
-                # otherwise, compute the thickness of the line and
-                # draw the connecting lines
-                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                # check to see if enough points have been accumulated in
-                # the buffer
-                if counter >= 10 and i == 10 and pts[-10] is not None:
-                    # compute the difference between the x and y
-                    # coordinates and re-initialize the direction
-                    # text variables
-                    dX = pts[-10][0] - pts[i][0]
-                    dY = pts[-10][1] - pts[i][1]
-                    (dirX, dirY) = ("", "")
-                    
-                    if(fsm == 1):       # check if active
-                        print("in state 1")
-                        if(dX > 10):
-                            fsm = 2;
-                    elif(fsm == 2):     # check if descending
-                        print("in state 2")
-                        if(dY > 10):
-                            fsm = 3;
-                    elif(fsm == 3):     # if ascending, return GOOD
-                        print("in state 3")
-                        if(dY < -10):
-                            # socket for outgoing messages
-                            socketOut = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-                            socketOut.connect((HOST, PORT + 1));
-                            print("hit")
-                            socketOut.send(b'GOOD\n');
-                            fsm = 0;    # HIT!
-                            pts = deque(maxlen = args["buffer"])
-                    
-
-                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-                        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.35, (0, 0, 255), 1)
-
-            # show the frame to our screen
-            cv2.imshow("Frame", mask)
-            border = cv2.copyMakeBorder(
-                frame,
-                top = BORDERWIDTH,
-                bottom = BORDERWIDTH,
-                left = BORDERWIDTH,
-                right = BORDERWIDTH,
-                borderType = cv2.BORDER_CONSTANT,
-                value = RED)
-            cv2.imshow('border', border)
-           ## cv2.imshow("shape",thresh)
-            key = cv2.waitKey(1) & 0xFF
-            counter += 1
-
-            # if the 'q' key is pressed, stop the l3oop
-            if key == ord("q"):
-                break
+cv2.putText(frame, txt, (150,160), cv2.FONT_HERSHEY_SIMPLEX, 2, borderColour, 2)
+bbox = cv2.selectROI(frame, False)
+# cap.release()
+# cv2.destroyAllWindows()
