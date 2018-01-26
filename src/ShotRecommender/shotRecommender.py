7c2b57f5da166f3deb94f6b53a9ac00d245423e8
Harit Patel <patelhp3@mcmaster.ca>
2018-01-25 21:38:01 -0500

    Shot Recommender v1.0 with Flask

---
+++ src/ShotRecommender/shotRecommender.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+X = [0.191,0.572,0.953,1.334]
+Y = [0.171,0.514,0.856,1.199]
+#V, W are subject to change
+V = [60, 65, 70, 75, 80]
+W = [-5, -10, -15, 5, 10, 15]
+
+@app.route('/')
+def helloWorld():
+	return 'Hello, World!'
+
+@app.route('/nextShot')
+def getNextShot():
+	outputString = 'X='+str(random.choice(X))+',Y='+str(random.choice(Y))+',V='+str(random.choice(V))+',W='+str(random.choice(W))
+	print outputString
+	return outputString
+
+app.run(host="127.0.0.1", port=8080)
