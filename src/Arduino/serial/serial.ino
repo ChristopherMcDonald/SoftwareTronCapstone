454abf3686465d24d9fc0bedbb35c3d29d798695
Christopher McDonald <mail@christophermcdonald.me>
2018-01-25 21:54:36 -0500

    arduino working

---
+++ src/Arduino/serial/serial.ino
@@ -0,0 +1,37 @@
+String data;        //variable to store incoming data from JAVA
+
+int LEDpin = 12;  //pin number LED is connected to
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+}
+
+void loop() {
+  if(Serial.available()>0){ //if data has been written to the Serial stream
+    data=Serial.readString();                 // ex. P=45,Y=-123,Z=1
+    int comma1 = data.indexOf(',');           // ex. 4
+    String val1 = data.substring(0,comma1);   // ex. P=45
+
+    data = data.substring(comma1 + 1);        // Y=-123,Z=1
+    int comma2 = data.indexOf(',');           // 6
+    String val2 = data.substring(0,comma2);   // Y=-123
+
+    data = data.substring(comma2 + 1);        // Z=1
+
+    double pitch = val1.substring(2).toDouble();
+    double yaw = val2.substring(2).toDouble();
+    double vel = data.substring(2).toDouble();
+
+    // works for values 45, -123, 1.2344354353534534432234324
+
+    //if(pitch != 12.30) {
+    while(true) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    //} else {
+      //digitalWrite(LED_BUILTIN, LOW);
+    //}
+  }
+}
--- src/SmartServe/src/controllers/ArduinoController.java
+++ src/SmartServe/src/controllers/ArduinoController.java
@@ -2,25 +2,31 @@ package controllers;
 
 import java.io.IOException;
 import adt.Position;
+import arduino.Arduino;
 
 public class ArduinoController {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		ArduinoController ac = new ArduinoController();
-		ac.shoot(12.3f, 12345.2f, 0.11232f);
+		ac.test("cu.usbmodem14431");
+		ac.shoot(12.3f, 12345.2f, 1.0f);
 	}
 	
-	private int port; // holds the port if successfully connects
+	private Arduino arduino; // holds the port if successfully connects
 	
 	/**
 	 * attempts to connect to Arduino over port <code>port</code>
 	 * @param port - port to connect over to arduino
 	 * @return successful connection
 	 */
-	public boolean test(int port) {
-		// TODO implement
-		this.port = port;
-		return false;
+	public boolean test(String port) {
+		arduino = new Arduino(port, 9600);
+		if(arduino.openConnection()) {
+			return true; 
+		} else {
+			arduino = null;
+			return false;
+		}
 	}
 	
 	/**
@@ -32,8 +38,13 @@ public class ArduinoController {
 	 * @throws InterruptedException 
 	 */
 	public void shoot(float pitch, float yaw, float angularVelocity) throws IOException, InterruptedException {
-		Process p = Runtime.getRuntime().exec("bash ../Arduino/shoot.sh " + pitch + " " + yaw + " " + angularVelocity);
-		p.waitFor();
+		String toSend = String.format("P=%.0f,Y=%.0f,Z=%.0f", pitch, yaw, angularVelocity);
+		
+		// DEBUGGING
+		System.out.println(toSend);
+		
+		arduino.serialWrite(toSend); 
+		arduino.closeConnection();
 	}
 	
 	/**
