a3cc3931da75f563b3f76f633452a971394f0ed0
Sharon <platkis@mcmaster.ca>
2018-01-29 13:14:18 -0500

    more UI

--- src/SmartServe/src/ui/View.java
+++ src/SmartServe/src/ui/View.java
@@ -8,7 +8,7 @@ import alt.*;
 public class View {
 	
 	private Controller control;
-
+	private static boolean paused = true;		
 	public static void main(String[] args) {  
 		
 		JFrame f = new JFrame();				//creating instance of JFrame  
@@ -17,15 +17,21 @@ public class View {
 		t.start();
 
 		JButton startBtn = new JButton("Start");		//creating instance of JButton  
-		startBtn.setBounds(50,100,100, 40);			//x axis, y axis, width, height  
+		startBtn.setBounds(50,100,100, 40);				//x axis, y axis, width, height  
 		
 		JButton pauseBtn = new JButton("Pause");		//creating instance of JButton  
 		pauseBtn.setBounds(150,100,100, 40);			//x axis, y axis, width, height 
+							//is it paused?
 		
-		JButton stopBtn = new JButton("Stop");		//creating instance of JButton  
-		stopBtn.setBounds(250,100,100, 40);			//x axis, y axis, width, height  
-
-		f.add(startBtn);								//adding button in JFrame  
+		JButton stopBtn = new JButton("Stop");			//creating instance of JButton  
+		stopBtn.setBounds(250,100,100, 40);				//x axis, y axis, width, height  
+		
+		String[] modes = { "Training","Random"};		//training modes
+	    final JComboBox<String> modeDropDown = new JComboBox<String>(modes); //creating instance of JComboBox(Dropdown)
+	    modeDropDown.setBounds(150, 150, 100,40);		//x acis, y axis, width, height
+	    
+	    f.add(modeDropDown);							//add JComboBox in JFrame
+		f.add(startBtn);								//adding buttons in JFrame  
 		f.add(pauseBtn);
 		f.add(stopBtn);
 
@@ -33,29 +39,47 @@ public class View {
 		f.setLayout(null);						//using no layout managers  
 		f.setVisible(true);						//making the frame visible  
 		
-		startBtn.addActionListener(new ActionListener() {
+		startBtn.setEnabled(true);				//start button enabled
+        pauseBtn.setEnabled(false);				//pause button disabled
+        stopBtn.setEnabled(false);  			//stop button disabled
+        modeDropDown.setEnabled(true);			//mode drop down enabled
+
+		
+		startBtn.addActionListener(new ActionListener() { // buttons active when start is pressed
 		     public void actionPerformed(ActionEvent ae) {
 		        startBtn.setEnabled(false);
 		        pauseBtn.setEnabled(true);
 		        stopBtn.setEnabled(true);  
+		        modeDropDown.setEnabled(false);
 		     }
 		   }
 		 );
 		
-		pauseBtn.addActionListener(new ActionListener() {
+		pauseBtn.addActionListener(new ActionListener() { //buttons active when pause is pressed
 		     public void actionPerformed(ActionEvent ae) {
 		        startBtn.setEnabled(false);
 		        pauseBtn.setEnabled(true);
 		        stopBtn.setEnabled(true);  
+		        modeDropDown.setEnabled(false);
+		        
+		        if(paused) { 				//if pause then continue
+		        	paused = false;
+		        	pauseBtn.setText("Continue");
+		        }
+		        else {						//if continue then pause
+		        	paused = true;
+		        	pauseBtn.setText("Pause");
+		        }
 		     }
 		   }
 		 );
 		
-		stopBtn.addActionListener(new ActionListener() {
+		stopBtn.addActionListener(new ActionListener() { //buttons active when stop is pressed
 		     public void actionPerformed(ActionEvent ae) {
 		        startBtn.setEnabled(true);
 		        pauseBtn.setEnabled(false);
 		        stopBtn.setEnabled(false);  
+		        modeDropDown.setEnabled(true);
 		     }
 		   }
 		 );
