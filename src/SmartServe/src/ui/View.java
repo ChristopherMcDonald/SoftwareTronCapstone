4684a8d85dc400773591ce1e6f16b1088a0b51c5
Sharon <platkis@mcmaster.ca>
2018-01-29 12:50:43 -0500

    UI buttons

--- src/SmartServe/src/ui/View.java
+++ src/SmartServe/src/ui/View.java
@@ -1,11 +1,14 @@
 package ui;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;  
 import alt.*;
 
 public class View {
 	
 	private Controller control;
-	
+
 	public static void main(String[] args) {  
 		
 		JFrame f = new JFrame();				//creating instance of JFrame  
@@ -13,21 +16,48 @@ public class View {
 		Thread t = new Thread(c);
 		t.start();
 
-		JButton b = new JButton("Start");		//creating instance of JButton  
-		b.setBounds(130,100,100, 40);			//x axis, y axis, width, height  
+		JButton startBtn = new JButton("Start");		//creating instance of JButton  
+		startBtn.setBounds(50,100,100, 40);			//x axis, y axis, width, height  
 		
-		JButton b1 = new JButton("Pause");		//creating instance of JButton  
-		b.setBounds(180,100,100, 40);			//x axis, y axis, width, height 
+		JButton pauseBtn = new JButton("Pause");		//creating instance of JButton  
+		pauseBtn.setBounds(150,100,100, 40);			//x axis, y axis, width, height 
 		
-		JButton b2 = new JButton("Stop");		//creating instance of JButton  
-		b.setBounds(230,100,100, 40);			//x axis, y axis, width, height  
+		JButton stopBtn = new JButton("Stop");		//creating instance of JButton  
+		stopBtn.setBounds(250,100,100, 40);			//x axis, y axis, width, height  
 
-		f.add(b);								//adding button in JFrame  
-		f.add(b1);
-		f.add(b2);
+		f.add(startBtn);								//adding button in JFrame  
+		f.add(pauseBtn);
+		f.add(stopBtn);
 
 		f.setSize(400,500);						//400 width and 500 height  
 		f.setLayout(null);						//using no layout managers  
 		f.setVisible(true);						//making the frame visible  
+		
+		startBtn.addActionListener(new ActionListener() {
+		     public void actionPerformed(ActionEvent ae) {
+		        startBtn.setEnabled(false);
+		        pauseBtn.setEnabled(true);
+		        stopBtn.setEnabled(true);  
+		     }
+		   }
+		 );
+		
+		pauseBtn.addActionListener(new ActionListener() {
+		     public void actionPerformed(ActionEvent ae) {
+		        startBtn.setEnabled(false);
+		        pauseBtn.setEnabled(true);
+		        stopBtn.setEnabled(true);  
+		     }
+		   }
+		 );
+		
+		stopBtn.addActionListener(new ActionListener() {
+		     public void actionPerformed(ActionEvent ae) {
+		        startBtn.setEnabled(true);
+		        pauseBtn.setEnabled(false);
+		        stopBtn.setEnabled(false);  
+		     }
+		   }
+		 );
 	}  
 }  
