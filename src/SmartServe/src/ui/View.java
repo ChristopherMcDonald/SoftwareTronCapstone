d16ad1ad743d8fa2e78b713b2cfe509070811fb6
Christopher McDonald <mail@christophermcdonald.me>
2018-01-29 11:15:41 -0500

    some ui stuff

--- src/SmartServe/src/ui/View.java
+++ src/SmartServe/src/ui/View.java
@@ -1,5 +1,33 @@
 package ui;
+import javax.swing.*;  
+import alt.*;
 
 public class View {
+	
+	private Controller control;
+	
+	public static void main(String[] args) {  
+		
+		JFrame f = new JFrame();				//creating instance of JFrame  
+		Controller c = new Controller();
+		Thread t = new Thread(c);
+		t.start();
 
-}
+		JButton b = new JButton("Start");		//creating instance of JButton  
+		b.setBounds(130,100,100, 40);			//x axis, y axis, width, height  
+		
+		JButton b1 = new JButton("Pause");		//creating instance of JButton  
+		b.setBounds(180,100,100, 40);			//x axis, y axis, width, height 
+		
+		JButton b2 = new JButton("Stop");		//creating instance of JButton  
+		b.setBounds(230,100,100, 40);			//x axis, y axis, width, height  
+
+		f.add(b);								//adding button in JFrame  
+		f.add(b1);
+		f.add(b2);
+
+		f.setSize(400,500);						//400 width and 500 height  
+		f.setLayout(null);						//using no layout managers  
+		f.setVisible(true);						//making the frame visible  
+	}  
+}  
