dcffcd25c663a4481dcbe2d6fda3509f8b8c8593
nishp763 <nishp763@gmail.com>
2018-01-29 18:49:22 -0500

    Classes working

--- src/Arduino/AutomaticPanning/AutomaticPanning.h
+++ src/Arduino/AutomaticPanning/AutomaticPanning.h
@@ -1,9 +1,14 @@
+#ifndef AutomaticPanning_h
+#define AutomaticPanning_h
+
+#include "Arduino.h"
+
 class AutomaticPanning
 {
     public:
         AutomaticPanning();
-        void home_azimuth(String motor_direction);
-
-    private:
-        void move_azimuth(String motor_direction, double azimuth_move_degrees);
+        void move_to_location(String motor_direction, double degrees_to_move);
+        void home_azimuth(String motor_direction);  	
 };
+
+#endif
