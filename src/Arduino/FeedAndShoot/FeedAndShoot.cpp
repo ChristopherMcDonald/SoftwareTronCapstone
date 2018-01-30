1f387fab31928953575fd3f08d1ec4010c84b3af
nishp763 <nishp763@gmail.com>
2018-01-30 16:13:16 -0500

    Created FeedAndShoot Class, class creating and compilation tested. Moving into adding methods

---
+++ src/Arduino/FeedAndShoot/FeedAndShoot.cpp
@@ -0,0 +1,7 @@
+#include "Arduino.h"
+#include "FeedAndShoot.h"
+
+FeedAndShoot::FeedAndShoot() // Constructor
+{
+	Serial.begin(9600);
+}
---
+++ src/Arduino/feed_and_shoot_tester/feed_and_shoot_tester/feed_and_shoot_tester.ino
@@ -0,0 +1,13 @@
+#include <FeedAndShoot.h>
+
+FeedAndShoot* myfeedandshoot = new FeedAndShoot();
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
