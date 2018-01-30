e7885a8b1291a0f1341f796d9be72d51357a51a6
nishp763 <nishp763@gmail.com>
2018-01-30 16:21:58 -0500

    Corrected Proof Of Concept code from merging problem

--- src/Arduino/FeedAndShoot/FeedAndShoot.cpp
+++ src/Arduino/FeedAndShoot/FeedAndShoot.cpp
@@ -1,7 +1,11 @@
 #include "Arduino.h"
 #include "FeedAndShoot.h"
+#include "Stepper.h"
 
 FeedAndShoot::FeedAndShoot() // Constructor
 {
 	Serial.begin(9600);
+
+	// Initialize Stepper Motor Pins
+	Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 }
