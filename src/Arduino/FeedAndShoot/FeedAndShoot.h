8ca3fc5d019ba624476b3222ff5158fb0a299cde
nishp763 <nishp763@gmail.com>
2018-01-30 17:28:47 -0500

    DC motor class stuff working perfect

--- src/Arduino/FeedAndShoot/FeedAndShoot.h
+++ src/Arduino/FeedAndShoot/FeedAndShoot.h
@@ -7,8 +7,14 @@ class FeedAndShoot
 {
 	public:
 		FeedAndShoot();
+		int get_dcspeed();
+		bool set_dcspeed(int);
+		//int get_stepperspeed();
 
-	//private:
+	private:
+		int stepperspeed, dcspeed;
+		
+		//void set_stepperspeed(int);
 
 
 };
