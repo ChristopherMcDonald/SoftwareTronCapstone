31a8d54a71daf59e4f21be66b7109092c16f9c57
hamels2 <sam2hamel@gmail.com>
2018-01-30 15:44:50 -0500

    fixed pts index out of bounds

--- src/ComputerVision/cv.py
+++ src/ComputerVision/cv.py
@@ -40,7 +40,7 @@ fsm = 0 # 0 - waiting request, 1 - detecting, 2 - active ball, 3 - descent
 
 # global variables
 HOST = "localhost";
-PORT = 9003;
+PORT = 8013;
 
 socketIn = socket.socket();             # handles incoming requests
 socketIn.bind((HOST, PORT));            # binds to a host and port
@@ -155,15 +155,16 @@ while(True):
             for i in np.arange(1, len(pts)):
                 # if either of the tracked points are None, ignore
                 # them
-                if pts[i - 1] is None or pts[i] is None:
-                    continue
+                if(fsm!=0):
+                    if pts[i - 1] is None or pts[i] is None:
+                        continue
 
-                # otherwise, compute the thickness of the line and
-                # draw the connecting lines
-                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                # check to see if enough points have been accumulated in
-                # the buffer
+                    # otherwise, compute the thickness of the line and
+                    # draw the connecting lines
+                    thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+                    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+                    # check to see if enough points have been accumulated in
+                    # the buffer
                 if counter >= 10 and i == 10 and pts[-10] is not None:
                     # compute the difference between the x and y
                     # coordinates and re-initialize the direction
@@ -191,12 +192,14 @@ while(True):
                             fsm = 0;    # HIT!
                             pts = deque(maxlen = args["buffer"])
 
+                if(fsm!=0):
+                    thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+                    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+                    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.35, (0, 0, 255), 1)
 
-                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-                        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.35, (0, 0, 255), 1)
+
 
             # show the frame to our screen
             cv2.imshow("Frame", mask)
