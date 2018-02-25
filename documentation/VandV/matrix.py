a0b55412fb623e0b47d8d3afd733c1acf3b90525
Christopher McDonald <mail@christophermcdonald.me>
2018-02-25 13:34:26 -0500

    forgot something

--- documentation/VandV/matrix.py
+++ documentation/VandV/matrix.py
@@ -1,7 +1,7 @@
 # RUN with python2
 import csv
 
-with open('data-f.csv', 'rb') as csvfile:
+with open('data-nf.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print "&".join(row[0].split(",")) + " \\\ \hline";
