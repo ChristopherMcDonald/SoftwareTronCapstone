387d82e5aaacb93b0e69fc78cbd73dae7e6728dc
Sharon <platkis@mcmaster.ca>
2018-02-25 17:33:30 -0500

    user interface

---
+++ src/UserInterface/UserInterface/src/Welcome.java
@@ -0,0 +1,64 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Welcome extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Welcome frame = new Welcome();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Welcome() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnSignUp = new JButton("Sign Up");
+		btnSignUp.setBounds(159, 62, 89, 23);
+		btnSignUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.welcomef.setVisible(false);
+				View.signupf.setVisible(true);
+			}
+		});
+		contentPane.add(btnSignUp);
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.setBounds(159, 97, 89, 23);
+		btnSignUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.welcomef.setVisible(false);
+				View.loginf.setVisible(true);
+			}
+		});
+		contentPane.add(btnLogIn);
+	}
+}
