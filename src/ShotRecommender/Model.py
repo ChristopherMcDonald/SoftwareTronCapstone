1649478eca8ff57b2acdf5601d3eceb535ec5af4
Christopher McDonald <mail@christophermcdonald.me>
2018-03-01 21:53:32 -0500

    first crack at SR

---
+++ src/ShotRecommender/Model.py
@@ -0,0 +1,7 @@
+from BinarySearch import BinarySearch;
+
+class Model:
+
+    def __init__(self, playerId):
+        self.id = playerId;
+        self.shots = BinarySearch();
--- src/ShotRecommender/shotRecommender.py
+++ src/ShotRecommender/shotRecommender.py
@@ -3,15 +3,19 @@ from flask import request
 import random
 import mysql.connector
 from mysql.connector import Error
- 
+from Model import Model;
+
 
 app = Flask(__name__)
 
 shot = random.randint(2, 1585)
+model = None;
 
-@app.route('/')
-def helloWorld():
-	return 'Hello, World!'
+@app.route('/start', methods = ['POST'])
+def start():
+    playerId = request.args.get("player");
+    model = Model(id);
+
 
 # takes in argument mode, ex. http://locahost:8080/nextShot?mode=#######
 @app.route('/nextShot', methods = ['POST'])
@@ -21,10 +25,11 @@ def getNextShot():
 								database='smartserve',
 								user='root',
 								password='smarTserve91',
-								port=3306)
+								port=3306);
+    shotId = model.shots.getNext();
 	if conn.is_connected():
-		print('Connected to MySQL database')
-		values = callProc(conn)
+		print('Connected to MySQL database');
+		values = callProc(conn, shotId);
 		outputString = 'X='+str(values[1])+',Y='+str(values[2])+',V='+str(values[3])+',W='+str(values[4])
 	print(outputString)
 
@@ -32,7 +37,7 @@ def getNextShot():
 	return outputString
 
 
-def callProc(conn):
+def callProc(conn, id):
 	cur = conn.cursor()
 
 	#Calling proc
@@ -45,7 +50,7 @@ def callProc(conn):
 	#for user_name, password in cur.fetchall() :
 	#	return (user_name + password)
 
-	
-  
+
+
 
 app.run(host="localhost", port=8080)
