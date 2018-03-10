62ec5bc8f933c7a455eae9394d4a3cb02266ad31
Sharon <platkis@mcmaster.ca>
2018-03-10 14:25:12 -0500

    login

---
+++ src/SmartServe/src/ui/Login.java
@@ -0,0 +1,102 @@
+package ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import controllers.SQLConnector;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField userNameInput;
+	private JTextField passwordInput;
+	
+
+	int user_id;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Email");
+		lblNewLabel.setBounds(118, 93, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(118, 117, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		userNameInput = new JTextField();
+		userNameInput.setBounds(226, 81, 86, 20);
+		contentPane.add(userNameInput);
+		userNameInput.setColumns(10);
+		
+		passwordInput = new JTextField();
+		passwordInput.setBounds(226, 106, 86, 20);
+		contentPane.add(passwordInput);
+		passwordInput.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(166, 153, 89, 23);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Object[] loginObj = new Object[]{userNameInput.getText(), passwordInput.getText()};
+				try {
+					ResultSet rs = SQLConnector.query("login_proc",loginObj);
+					rs.next();
+					user_id = rs.getInt(1);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				View.loginf.setVisible(false);
+				View.profilef.setVisible(true);
+			}
+		});
+		
+		contentPane.add(btnNewButton);
+		JLabel lblLogIn = new JLabel("Log In");
+		lblLogIn.setFont(new Font("Century", Font.PLAIN, 35));
+		lblLogIn.setBounds(149, 4, 137, 69);
+		contentPane.add(lblLogIn);
+
+	}
+}
---
+++ src/SmartServe/src/ui/Profile.java
@@ -0,0 +1,99 @@
+package ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Choice;
+import javax.swing.JButton;
+
+public class Profile extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Profile frame = new Profile();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Profile() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblProfile = new JLabel("Profile");
+		lblProfile.setFont(new Font("Century", Font.PLAIN, 35));
+		lblProfile.setBounds(149, 4, 137, 69);
+		contentPane.add(lblProfile);
+		
+		JLabel lblUserNameSharon = new JLabel("User Name: Sharon Platkin");
+		lblUserNameSharon.setBounds(144, 84, 142, 14);
+		contentPane.add(lblUserNameSharon);
+		
+		JButton btnChangePassword = new JButton("Change Password");
+		btnChangePassword.setBounds(149, 110, 119, 23);
+		contentPane.add(btnChangePassword);
+		
+		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Log Out Successful", "Log Out", JOptionPane.PLAIN_MESSAGE);
+				View.profilef.setVisible(false);
+				View.welcomef.setVisible(true);
+			}
+		});
+		btnLogOut.setBounds(159, 144, 89, 23);
+		contentPane.add(btnLogOut);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenuItem mntmProfile = new JMenuItem("Profile");
+		menuBar.add(mntmProfile);
+		
+		JMenuItem mntmControl = new JMenuItem("Control");
+		mntmControl.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.profilef.setVisible(false);
+				View.controlf.setVisible(true);
+			}
+		});
+		menuBar.add(mntmControl);
+		
+		JMenuItem mntmStatistics = new JMenuItem("Statistics");
+		mntmStatistics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.profilef.setVisible(false);
+				View.statsf.setVisible(true);
+			}
+		});
+		menuBar.add(mntmStatistics);
+		
+	}
+}
