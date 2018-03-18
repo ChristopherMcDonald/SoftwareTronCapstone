df9c8949de21b0ee739f288a17d6889ee83c2dfc
nishp763 <nishp763@gmail.com>
2018-03-17 23:01:40 -0400

    Added Automatic Roll header files, still need to update the code

---
+++ src/Arduino/AutomaticRoll/AutomaticRoll.h
@@ -0,0 +1,20 @@
+#ifndef AutomaticRoll_h
+#define AutomaticRoll_h
+
+#include "Arduino.h"
+
+class AutomaticRoll
+{
+    public:
+        AutomaticRoll();
+        bool move_to_location(double desired_location);
+        bool home_assembly(String motor_direction); 
+        double get_current_location();
+    private:
+    	// Home Position -> 0 deg and measured CCW+
+    	double current_location, target_location, location_diff; 
+    	void set_current_location(double location);
+    	void move_by_degrees(String motor_direction, double degrees_to_move);
+};
+
+#endif
