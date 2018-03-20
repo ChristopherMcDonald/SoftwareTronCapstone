b124d39aeed0b7bdeeb38867772c999d13cc0d4c
nishp763 <nishp763@gmail.com>
2018-03-20 17:32:13 -0400

    Added header and cpp files for Pitchg

---
+++ src/Arduino/Pitch/Pitch.h
@@ -0,0 +1,19 @@
+#ifndef Pitch_h
+#define Pitch_h
+
+#include "Arduino.h"
+
+class Pitch
+{
+	public:
+		Pitch();
+		bool move_to_location(double desired_location);
+		bool home_assembly();
+		double get_current_location();
+
+	private:
+		double current_location, target_location;
+		void set_current_location(double location);
+};
+
+#endif
