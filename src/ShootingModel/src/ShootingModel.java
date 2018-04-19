261ca92e8e218bdb09bb90b26039aac69bc87510
Harit Patel <patelhp3@mcmaster.ca>
2018-04-19 19:01:37 -0400

    Shooting Model with drag - v2.0

--- src/ShootingModel/src/ShootingModel.java
+++ src/ShootingModel/src/ShootingModel.java
@@ -1,24 +1,29 @@
 /**
  * Represents the Shooting Mechanism, used for determining valid shot parameters 
  * @author Harit Patel
- * @version 1.1
- *
- * TODO items:
- * - add Calculate Y Plane Angle method
+ * @version 2.0
  */
 public class ShootingModel {
-
-	// Instantiate necessary variables
-	public double xCoord;
-	public double yCoord;
-	public double distance;
-	public double xDistance;
-	public double yDistance;
-	public double yaw;
+	
+	public double xInitialDistance = 0;
 	public double yInitialHeight;
 	public double pitch;
+	public double xDistance;
+	public double yDistance;
+	public double distance;
 	public double gravity = 9.8;
-	public double velocity;
+
+
+	/**
+	 * ShootingModel Constructor
+	 * @param initialHeight [Private static var to hold height of shooter]
+	 * @param landingXCoord [X Coordinate on the grid for landing position]
+	 * @param landingYCoord [X Coordinate on the grid for landing position]
+	 */
+	public ShootingModel(double initialHeight, double pitch){
+		this.yInitialHeight = initialHeight;
+		this.pitch = pitch;
+	}
 
 	/**
 	 * Calculates the distance in x plane from the shooter to the landing point
@@ -26,7 +31,7 @@ public class ShootingModel {
 	 * @return xDist [Distance (meters) the ball must travel in X direction]
 	 */
 	private double calculateXDistance(double landingXCoord){
-		//The 0.38125 is the centre of the board in x-plane; location of the shooter.
+		//The 0.7625 is the centre of the board in x-plane; location of the shooter.
 		double xDist = Math.abs(0.7625 - landingXCoord);
 		return xDist;
 	}
@@ -38,7 +43,7 @@ public class ShootingModel {
 	 */
 	private double calculateYDistance(double landingYCoord){
 		//The location of the shooter is at 0 m from the edge
-		double yDist = Math.abs(landingYCoord - 0);
+		double yDist = Math.abs(landingYCoord + 1.37 + xInitialDistance);
 		return yDist;
 	}
 
@@ -50,8 +55,6 @@ public class ShootingModel {
 	 * @return totalDistance [Total distance (meters) the ball must travel]
 	 */
 	public double calculateDistance(double landingXCoord, double landingYCoord){
-		xCoord = landingXCoord;
-		yCoord = landingYCoord;
 		xDistance = calculateXDistance(landingXCoord);
 		yDistance = calculateYDistance(landingYCoord);
 		double distSquared = Math.pow(xDistance, 2) + Math.pow(yDistance, 2);
@@ -66,7 +69,7 @@ public class ShootingModel {
 	 * @param xDist [Distance (meters) the ball must travel in X direction]
 	 * @return yawAngle [Yaw angle (degrees)]
 	 */
-	private double calculateYawAngle(double xDist, double yDist){
+	private double calculateYawAngle(double xDist, double yDist, double xCoord){
 		double yawAngle = Math.toDegrees(Math.atan(yDist/xDist));
 		if (xCoord <= 0.7625) {
 			yawAngle = 180-yawAngle;
@@ -79,23 +82,35 @@ public class ShootingModel {
 	 * Calculates velocity in Yaw direction
 	 * @return vel [Velocity (meters/second)]
 	 */
-	public double calculateVelocity(){
-		double vel = Math.sqrt((Math.pow(distance, 2)*gravity)/(2*Math.pow(Math.cos(pitch),2)*(yInitialHeight+distance*Math.tan(pitch))));
+	public double calculateVelocity() {
+		double vel = Math.sqrt(gravity/(2.0*Math.tan(pitch)*distance-2.0*yInitialHeight))*(distance/Math.cos(pitch));
 		System.out.println("Velocity: "+vel+" m/s");
 		return vel;
 	}
-	
-	/**
-	 * ShootingModel Constructor
-	 * @param initialHeight [Private static var to hold height of shooter]
-	 * @param landingXCoord [X Coordinate on the grid for landing position]
-	 * @param landingYCoord [X Coordinate on the grid for landing position]
-	 */
-	public ShootingModel(double initialHeight, double pitch){
-		yInitialHeight = initialHeight;
-		this.pitch = pitch;
+
+	public double calculateVelocityWithDrag(double initVelocity) {
+
+		double timeIncrement = 0.1;
+		double p = 1.0;
+		double dragConstant = 0.0014362336;
+		double area = Math.PI*(0.02*0.02);
+
+		double airTime = distance/(initVelocity*Math.cos(pitch));
+		double velocityWithDrag = initVelocity;
+		double flag = 0.0;
+
+		//TODO: Add while loop for getting effects of drag on velocity
+		while (flag <= airTime) {
+			flag = flag + timeIncrement;
+			double theta = Math.acos(distance/(initVelocity*flag));
+			double drag = 0.5*p*area*dragConstant*Math.pow(initVelocity, 2)*Math.sin(theta);
+			velocityWithDrag = velocityWithDrag - gravity*timeIncrement;
+			velocityWithDrag = velocityWithDrag + drag;			
+		}
+
+		return velocityWithDrag;
 	}
-	
+
 	/**
 	 * Method to get Yaw,Velocity from abstract data type (ShootingDetails)
 	 * @param landingXCoord [X Coordinate on the grid for landing position]
@@ -104,9 +119,10 @@ public class ShootingModel {
 	 */
 	public ShootingDetails getShootingDetails(double landingXCoord, double landingYCoord){
 		distance = calculateDistance(landingXCoord, landingYCoord);
-		yaw = calculateYawAngle(xDistance, yDistance);
-		velocity = calculateVelocity();
-		return new ShootingDetails(yaw, velocity);
+		double yaw = calculateYawAngle(xDistance, yDistance, landingXCoord);
+		double velocity = calculateVelocity();
+		double dragVelocity = calculateVelocityWithDrag(velocity);
+		return new ShootingDetails(yaw, dragVelocity);
 	}
 
 	public static void main(String []args){
