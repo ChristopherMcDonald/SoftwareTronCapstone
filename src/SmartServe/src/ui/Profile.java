6bc69a0c2807bbc3ced6577963b075724c6b5dee
Sharon <platkis@mcmaster.ca>
2018-04-19 19:24:46 -0400

    graphs init

--- src/SmartServe/src/ui/Profile.java
+++ src/SmartServe/src/ui/Profile.java
@@ -13,13 +13,15 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.SystemColor;
+
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
 public class Profile extends JFrame {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -50,24 +52,24 @@ public class Profile extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblProfile = new JLabel("Profile");
 		lblProfile.setHorizontalAlignment(SwingConstants.CENTER);
 		lblProfile.setFont(new Font("Century", Font.PLAIN, 35));
 		lblProfile.setBounds(144, 4, 137, 69);
 		contentPane.add(lblProfile);
-		
+
 		JLabel lblUserName = new JLabel();
 		lblUserName.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblUserName.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUserName.setBounds(125, 84, 156, 14);
 		contentPane.add(lblUserName);
-		
+
 		JButton btnChangePassword = new JButton("Change Password");
 		btnChangePassword.setBounds(144, 109, 119, 23);
 		btnChangePassword.setMargin(new Insets(2, 2, 2, 2));
 		contentPane.add(btnChangePassword);
-		
+
 		JButton btnLogOut = new JButton("Log Out");
 		btnLogOut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -78,16 +80,21 @@ public class Profile extends JFrame {
 		});
 		btnLogOut.setBounds(154, 143, 89, 23);
 		contentPane.add(btnLogOut);
-		
+
 		//todo: change name
 		lblUserName.setText("Welcome " + "Sharon Platkin");
-		
+
+		setJMenuBar(createMenu());
+
+	}
+
+	public static JMenuBar createMenu() {
 		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
+
 		JMenuItem mntmProfile = new JMenuItem("Profile");
+		mntmProfile.setBackground(SystemColor.activeCaption);
 		menuBar.add(mntmProfile);
-		
+
 		JMenuItem mntmControl = new JMenuItem("Control");
 		mntmControl.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -96,7 +103,7 @@ public class Profile extends JFrame {
 			}
 		});
 		menuBar.add(mntmControl);
-		
+
 		JMenuItem mntmStatistics = new JMenuItem("Statistics");
 		mntmStatistics.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -105,7 +112,7 @@ public class Profile extends JFrame {
 			}
 		});
 		menuBar.add(mntmStatistics);
-		
+
 		JMenuItem mntmTests = new JMenuItem("Testing");
 		mntmTests.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -114,6 +121,8 @@ public class Profile extends JFrame {
 			}
 		});
 		menuBar.add(mntmTests);
-		
+
+        return menuBar;
+
 	}
 }
